0563fdbc20b947de11a972f54d93b8b9c7bf72d1
Roy <roy.wiersma@hva.nl>
2021-09-13 15:45:36 +0200

    Added header and welcome component

--- ElectricScooter/src/app/app.module.ts
+++ ElectricScooter/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule
