57036e6edb15b5fc0ecb4d8dedb2731d7a66d9d1
sweco-nlsall <salman.lartey@sweco.nl>
2021-09-15 13:58:47 +0200

    overview31 added

--- ElectricScooter/src/app/app.module.ts
+++ ElectricScooter/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HeaderComponent } from './components/header/header.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import '@angular/common/locales/global/nl';
+import {Overview31Component} from "./components/scooters/overview31/overview31.component";
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     WelcomeComponent,
-    NavBarComponent
+    NavBarComponent,
+    Overview31Component
   ],
   imports: [
     BrowserModule
