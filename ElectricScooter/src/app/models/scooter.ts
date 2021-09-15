55da7ffbd28b40e7872a6fa4b71ca2af415c5b01
Roy Wiersma <RoyWiersma@users.noreply.github.com>
2021-09-15 13:43:54 +0200

    Added scooter-status enum class

--- ElectricScooter/src/app/models/scooter.ts
+++ ElectricScooter/src/app/models/scooter.ts
@@ -1,8 +1,20 @@
+import {ScooterStatus} from "./scooter-status";
+
 export class Scooter {
-  // id: number;
-  // tag: string;
-  // status: ScooterStatus;
-  // gpsLocation: string;
-  // mileage: number;
-  // batteryCharge: number;
+  id: number;
+  tag: string;
+  status: ScooterStatus;
+  gpsLocation: string;
+  mileage: number;
+  batteryCharge: number;
+
+
+  constructor(id: number, tag: string, status: ScooterStatus, gpsLocation: string, mileage: number, batteryCharge: number) {
+    this.id = id;
+    this.tag = tag;
+    this.status = status;
+    this.gpsLocation = gpsLocation;
+    this.mileage = mileage;
+    this.batteryCharge = batteryCharge;
+  }
 }
