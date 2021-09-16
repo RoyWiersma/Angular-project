2e482b08252e7de89adf6096fd6ee8fa005806d2
Roy Wiersma <RoyWiersma@users.noreply.github.com>
2021-09-16 11:03:16 +0200

    Added random value generators

--- ElectricScooter/src/app/models/scooter.ts
+++ ElectricScooter/src/app/models/scooter.ts
@@ -16,8 +16,8 @@ export class Scooter {
   mileage: number;
   batteryCharge: number;
 
-  constructor(tag: string, status: ScooterStatus, gpsLocation: string, mileage: number, batteryCharge: number) {
-    this.id = Scooter.num;
+  constructor(id: number, tag: string, status: ScooterStatus, gpsLocation: string, mileage: number, batteryCharge: number) {
+    this.id = id;
     this.tag = tag;
     this.status = status;
     this.gpsLocation = gpsLocation;
@@ -25,31 +25,79 @@ export class Scooter {
     this.batteryCharge = batteryCharge;
   }
 
-  public static createSampleScooter(pId=0) : Scooter {
+  public static createSampleScooter(pId = 0): Scooter {
+    return new Scooter(
+      this.getRandomInt(30001000, 30001999, 0),
+      this.getRandomString(8),
+      this.getRandomStatus(),
+      this.getRandomAmsterdamGps(),
+      this.getRandomInt(0, 10000, 0),
+      this.getRandomInt(5, 100, 0)
+    );
 
-
-    // Random Tag ID
-    const tag: string = Math.random().toString(36).substring(2, 6) + Math.random().toString(36).substring(2, 6);
-
-
-    // Random amount of mileage
-    const mileage = Math.floor(Math.random() * (10000 - 2000) + 2000);
-
-
-    // Random ScooterState
-    const randomEnumValue = (ScooterStatus) => {
-      const values = Object.keys(enumeration);
-      const enumKey = values[Math.floor(Math.random() * values.length)];
-    };
-    //Random Status Value
-    const scooter = randomEnumValue(ScooterStatus);
-
-    // Random amount of battery charge
-    const batteryCharge = Math.floor(Math.random() * (100 - 5) + 5);
-
-    pId++;
-    // Instantiate new random scooter
-    // return new Scooter(tag, scooter, null, mileage, batteryCharge);
   }
 
+  private static getRandomString(length: number): string {
+    const randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+    }
+    return result;
+  }
+
+  // @ts-ignore
+  private static getRandomInt(from, to, fixed): number {
+    return (Math.random() * (to - from) + from).toFixed(fixed);
+  }
+
+  // private static getRandomAmsterdamGps(): string{
+  //   if (this.getRandomStatus() != ScooterStatus.IN_USE){
+  //     console.log(this.getRandomInt(52, 52.999999, 6) + ', ' + this.getRandomInt(4, 4.999999, 6))
+  //     return this.getRandomInt(52, 52.999999, 6) + ', ' + this.getRandomInt(4, 4.999999, 6);
+  //   }
+  //   else {
+  //     return "";
+  //   }
+  // }
+
+
+  // @ts-ignore
+  public static getRandomStatus(): ScooterStatus {
+    const status = this.getRandomInt(0, 3, 0)
+    // const values = Object.keys(ScooterStatus);
+    // const enumKey = values[Math.floor(Math.random() * values.length)]
+    console.log(status);
+    //
+    // return enumKey
+    switch (status) {
+      case 0: {
+        console.log('in use');
+        return ScooterStatus.IN_USE;
+        break;
+      }
+      case 1: {
+        console.log('idle');
+        return ScooterStatus.IDLE;
+        break;
+      }
+      case 2: {
+        console.log('maintenance');
+        return ScooterStatus.MAINTENANCE;
+        break;
+      }
+      default: {
+        console.log('default');
+      }
+    }
+  }
+
+  private static getRandomAmsterdamGps(): string {
+    console.log("getRandomAmsterdamGps" + this.getRandomStatus())
+    if (this.getRandomStatus() === 'IDLE') {
+      return this.getRandomInt(52, 52.999999, 6) + ', ' + this.getRandomInt(4, 4.999999, 6);
+    } else {
+      return "";
+    }
+  }
 }
