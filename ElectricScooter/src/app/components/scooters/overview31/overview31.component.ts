14d6c290ef93c08f36a585c39005a5c7c1a5cc18
Roy <roy.wiersma@hva.nl>
2021-09-21 14:47:10 +0200

    Added onNewScooter function

--- ElectricScooter/src/app/components/scooters/overview31/overview31.component.ts
+++ ElectricScooter/src/app/components/scooters/overview31/overview31.component.ts
@@ -16,8 +16,11 @@ export class Overview31Component implements OnInit {
   ngOnInit(): void {
     this.scooters=[];
     for (let i=0; i< 8; i++){
-      this.scooters.push(Scooter.createSampleScooter())
+      this.scooters.push(Scooter.createSampleScooter()
+      )
     }
   }
-
+  public onNewScooter(): void{
+    this.scooters[this.scooters.length] = Scooter.createSampleScooter();
+  }
 }
