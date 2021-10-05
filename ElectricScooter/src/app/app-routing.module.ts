5df9b20e85bd3a4a695444c2c5c13b8db2147762
Roy <roy.wiersma@hva.nl>
2021-10-05 16:09:53 +0200

    3.4b done

--- ElectricScooter/src/app/app-routing.module.ts
+++ ElectricScooter/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {WelcomeComponent} from "./components/welcome/welcome.component";
 import {Overview31Component} from "./components/scooters/overview31/overview31.component";
+import {Overview32Component} from "./components/scooters/overview32/overview32.component";
+import {Overview33Component} from "./components/scooters/overview33/overview33.component";
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home' , component: WelcomeComponent},
-  { path: 'overview31' , component: Overview31Component}
+  { path: 'overview31' , component: Overview31Component},
+  { path: 'overview32' , component: Overview32Component},
+  { path: 'overview33' , component: Overview33Component}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
