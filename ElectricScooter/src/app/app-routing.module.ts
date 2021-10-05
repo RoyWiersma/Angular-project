64934af992eec04fca892561a16c29c5c56b6007
Roy <roy.wiersma@hva.nl>
2021-10-05 17:19:13 +0200

    Added router with parameter

--- ElectricScooter/src/app/app-routing.module.ts
+++ ElectricScooter/src/app/app-routing.module.ts
@@ -5,7 +5,9 @@ import {Overview31Component} from "./components/scooters/overview31/overview31.c
 import {Overview32Component} from "./components/scooters/overview32/overview32.component";
 import {Overview33Component} from "./components/scooters/overview33/overview33.component";
 import {UnknownRouteComponent} from "./components/unknown-route/unknown-route.component";
-import {Overview34Component} from "./components/overview34/overview34.component";
+import {Overview34Component} from "./components/scooters/overview34/overview34.component";
+import {ScooterStartComponent} from "./components/scooters/scooter-start/scooter-start.component";
+import {Detail34Component} from "./components/scooters/detail34/detail34.component";
 
 
 const appRoutes: Routes = [
@@ -14,7 +16,10 @@ const appRoutes: Routes = [
   { path: 'overview31' , component: Overview31Component},
   { path: 'overview32' , component: Overview32Component},
   { path: 'overview33' , component: Overview33Component},
-  { path: 'overview34' , component: Overview34Component},
+  { path: 'overview34' , component: Overview34Component, children: [
+      {path: '', component: ScooterStartComponent},
+      {path: ':id' , component: Detail34Component}
+    ]},
   { path: 'not-found', component: UnknownRouteComponent},
   { path: '**',  redirectTo: '/not-found'}
 ];
